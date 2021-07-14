336ac69cd1cc6215ebfc8dfd9a48b4ad93a755b8
Agus Dwi Sasongko <agusdwisasongko1@gmail.com>
2021-07-14 18:19:42 +0700

    add seo with next-seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { NextSeo } from 'next-seo'
 
 const Home = (): JSX.Element => (
-  <div className="h-screen w-full">
-    <div className="h-full flex justify-center items-center">
-      <p className="text-red-500">I&lsquo;m Colored with TailwindCSS</p>
+  <>
+    <NextSeo
+      title="Next Starter Pack"
+      description="Your description"
+      openGraph={{
+        url: 'https://www.url.ie/a',
+        title: 'Open Graph Title',
+        description: 'Open Graph Description',
+        images: [
+          {
+            url: 'https://www.example.ie/og-image-01.jpg',
+            width: 800,
+            height: 600,
+            alt: 'Og Image Alt',
+          },
+          {
+            url: 'https://www.example.ie/og-image-02.jpg',
+            width: 900,
+            height: 800,
+            alt: 'Og Image Alt Second',
+          },
+          { url: 'https://www.example.ie/og-image-03.jpg' },
+          { url: 'https://www.example.ie/og-image-04.jpg' },
+        ],
+        site_name: 'SiteName',
+      }}
+    />
+    <div className="h-screen w-full">
+      <div className="h-full flex justify-center items-center">
+        <p className="text-red-700">I&lsquo;m Colored with TailwindCSS</p>
+      </div>
     </div>
-  </div>
+  </>
 )
 
 export default Home
