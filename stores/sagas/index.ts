b05ab3c4ffc7030008df7c307c3eddfae83d33f2
Agus Dwi Sasongko <agus.sasongko@stockbit.com>
2021-10-13 04:38:54 +0700

    clear: remove console

--- stores/sagas/index.ts
+++ stores/sagas/index.ts
@@ -6,6 +6,5 @@ import movieSagasWatcher from './movieSagas';
 // Export Combined Saga
 export default function* rootSaga() {
   // eslint-disable-next-line no-console
-  console.log('Redux Saga is Running!');
   yield all([spawn(movieSagasWatcher)]);
 }
